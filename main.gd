f21be354e9a0e3397101ab6d54a0f29192a950b6
Shuflduf <shuflduf@gmail.com>
2024-12-03 23:08:25 -0700

    update

--- main.gd
+++ main.gd
@@ -1,6 +1,12 @@
 extends Node2D
 
-var playing = false
+var playing = false:
+	set(value):
+		playing = value
+		$CanvasLayer/UI.label.visible = !value
+
+var generation_update = 1.0
+var time_generation = 0.0
 
 func _ready() -> void:
 	RenderingServer.set_default_clear_color(Color(0,0,0))
@@ -13,7 +19,14 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index in [MOUSE_BUTTON_WHEEL_DOWN, MOUSE_BUTTON_WHEEL_UP]:
 			var zoom_amount = -(event.button_index - 4.5)
-			$Camera.zoom = clamp($Camera.zoom + Vector2(zoom_amount, zoom_amount), Vector2(0.5, 0.5), Vector2(50, 50))
+			$Camera.zoom = clamp($Camera.zoom + Vector2(zoom_amount, zoom_amount), Vector2(0.5, 0.5), Vector2(100, 100))
+
+func _physics_process(delta: float) -> void:
+	if playing:
+		time_generation += delta
+		if time_generation >= generation_update:
+			update_cells()
+			time_generation = 0.0
 
 func _process(_delta: float) -> void:
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
@@ -26,4 +39,49 @@ func _process(_delta: float) -> void:
 
 	if Input.is_action_just_pressed("play"):
 		playing = !playing
-		print(playing)
+		print("Playing: ", playing)
+
+func update_cells():
+	var tilemap: TileMapLayer = $TileMap
+	var alive_cells = tilemap.get_used_cells()
+	var calculating_cell_pos = get_all_useful_cells()
+	var new_cell_pos: Array[Vector2i] = []
+	for cell in calculating_cell_pos:
+		print(cell)
+		var radius = 1
+		var square = range(-radius, radius + 1)
+		var neighbor_count = 0
+		for x in square:
+			for y in square:
+				var check_pos = cell + Vector2i(x, y)
+				print("Checking: ", check_pos)
+				if check_pos != cell:
+					if check_pos in alive_cells:
+						neighbor_count += 1
+		if cell in alive_cells:
+			if neighbor_count in range(2, 3 + 1):
+				new_cell_pos.append(cell)
+		else:
+			if neighbor_count == 3:
+				new_cell_pos.append(cell)
+	
+	tilemap.clear()
+	for i in new_cell_pos:
+		tilemap.set_cell(i, 0, Vector2i(0, 0))
+
+func get_all_useful_cells() -> Array[Vector2i]:
+	var tilemap: TileMapLayer = $TileMap
+	var alive_cells = tilemap.get_used_cells()
+	var final: Array[Vector2i] = alive_cells
+	for cell in alive_cells:
+		var radius = 1
+		var square = range(-radius, radius + 1)
+		for x in square:
+			for y in square:
+				var check_pos = cell + Vector2i(x, y)
+				print(check_pos)
+				if !check_pos in final:
+					final.append(check_pos)
+	
+	print(final)
+	return final
